9b0bfeb1d3417a663793e63c0dd2aabb6fe9fafa
MEvanAW <m.evan.aw@gmail.com>
2022-09-25 10:28:41 +0700

    Prints name, address, job, and reason

--- Biodata/biodata.go
+++ Biodata/biodata.go
@@ -6,10 +6,17 @@ import (
 	"strconv"
 )
 
+type teman struct {
+	nama      string
+	alamat    string
+	pekerjaan string
+	alasan    string
+}
+
 func main() {
 	args := os.Args
 	if len(args) <= 1 {
-		fmt.Println("There is no argument passed.")
+		fmt.Println("There is no argument passed. Valid argument range: 0-49")
 		return
 	}
 	arg, err := strconv.Atoi(args[1])
@@ -18,7 +25,75 @@ func main() {
 		return
 	}
 	if arg < 0 {
-		fmt.Println("Argument may not be negative.")
+		fmt.Println("Argument may not be negative. Valid argument range: 0-49")
 		return
+	}
+	if arg > 49 {
+		fmt.Println("Argument out of range. Valid argument range: 0-49")
+		return
+	}
+	temans := []teman{
+		{
+			"Muhammad Evan Anindya Wahyuaji",
+			"Sugiyapranata 8, Bareng Lor, Klaten Utara, Klaten, Jawa Tengah",
+			"Sedang mencari kerja",
+			"Untuk memperluas ilmu Teknologi Informasi saya",
+		}, {
+			"Faqih Yugos",
+			"Alamat 2",
+			"Pekerjaan 2",
+			"Alasan 2",
+		}, {
+			"Ilham Nopi Hendri",
+			"Alamat 3",
+			"Pekerjaan 3",
+			"Alasan 3",
+		}, {
+			"Ivan Muhammad Nizar",
+			"Alamat 4",
+			"Pekerjaan 4",
+			"Alasan 4",
+		}, {
+			"Laba",
+			"Alamat 5",
+			"Pekerjaan 5",
+			"Alasan 5",
+		}, {
+			"Michael Brian Bagaskara",
+			"Alamat 6",
+			"Pekerjaan 6",
+			"Alasan 6",
+		}, {
+			"Syahrul Apriansyah",
+			"Alamat 7",
+			"Pekerjaan 7",
+			"Alasan 7",
+		}, {
+			"Wira Ardi Kesuma Adji",
+			"Alamat 8",
+			"Pekerjaan 8",
+			"Alasan 8",
+		}, {
+			"Adi Ahmad Wijaya",
+			"Alamat 9",
+			"Pekerjaan 9",
+			"Alasan 9",
+		}, {
+			"Ahmad Aditya Gocendra Daud",
+			"Alamat 10",
+			"Pekerjaan 10",
+			"Alasan 10",
+		},
+	}
+	if arg > len(temans)-1 {
+		fmt.Println("Nama", arg)
+		fmt.Println("Alamat", arg)
+		fmt.Println("Pekerjaan", arg)
+		fmt.Println("Alasan", arg)
+	} else {
+		fmt.Println(temans[arg].nama)
+		fmt.Println(temans[arg].alamat)
+		fmt.Println(temans[arg].pekerjaan)
+		fmt.Println(temans[arg].alasan)
 	}
 }
