b05338f1cac98ec3de88120442cad02acbe39c55
M. Evan Anindya W <m.evan.aw@gmail.com>
2022-10-09 08:44:10 +0700

    Rename model fields to fit GORM convention.

--- OrderApi/models.go
+++ OrderApi/models.go
@@ -3,15 +3,15 @@ package models
 import "time"
 
 type Item struct {
-	ItemId      uint   `gorm:"primaryKey" json:"item_id"`
+	ID          uint   `gorm:"primaryKey" json:"item_id"`
 	ItemCode    string `gorm:"not null;type:varchar(8192)" json:"item_code"`
 	Description string `gorm:"type:varchar(8192)"`
 	Quantity    uint   `gorm:"not null"`
-	OrderId     uint   `gorm:"not null" json:"order_id"`
+	OrderID     uint   `gorm:"not null" json:"order_id"`
 }
 
 type Order struct {
-	OrderId      uint   `gorm:"primaryKey" json:"order_id"`
+	ID           uint   `gorm:"primaryKey" json:"order_id"`
 	CustomerName string `gorm:"type:varchar(8192)" json:"customer_name"`
 	Items        []Item
 	OrderedAt    time.Time `gorm:"not null" json:"ordered_at"`
