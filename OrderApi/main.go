197d036e144f586b3a0c6ddd12f9af214362a27c
M. Evan Anindya W <m.evan.aw@gmail.com>
2022-10-09 11:22:09 +0700

    add create order

--- OrderApi/main.go
+++ OrderApi/main.go
@@ -1,9 +1,40 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"time"
+
 	"excercise.id/orderapi/database"
+	"excercise.id/orderapi/models"
 )
 
 func main() {
 	database.StartDB()
+	items := []models.Item{
+		{
+			ItemCode: "100214",
+			Quantity: 1,
+		},
+		{
+			ItemCode:    "106310",
+			Description: "using promo",
+			Quantity:    3,
+		},
+	}
+	CreateOrder("Evan", items)
+}
+
+func CreateOrder(customerName string, items []models.Item) {
+	db := database.GetDB()
+	Order := models.Order{
+		CustomerName: customerName,
+		Items:        items,
+		OrderedAt:    time.Now(),
+	}
+	err := db.Create(&Order).Error
+	if err != nil {
+		fmt.Println("Error creating order data: ", err)
+	}
+	log.Println("New Order Data: ", Order)
 }
