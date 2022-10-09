720a691bddee323ffb2c4ed8b5a779762512cd41
M. Evan Anindya W <m.evan.aw@gmail.com>
2022-10-09 08:28:36 +0700

    Add gorm tag to models.

--- OrderApi/models.go
+++ OrderApi/models.go
@@ -3,15 +3,16 @@ package models
 import "time"
 
 type Item struct {
-	ItemId      uint   `json:"item_id"`
-	ItemCode    string `json:"item_code"`
+	ItemId      uint   `gorm:"primaryKey" json:"item_id"`
+	ItemCode    string `json:"item_code" gorm:"not null;type:varchar(8192)"`
 	Description string
-	Quantity    uint
-	OrderId     uint `json:"order_id"`
+	Quantity    uint `gorm:"not null"`
+	OrderId     uint `json:"order_id" gorm:"not null"`
 }
 
 type Order struct {
-	OrderId      uint      `json:"order_id"`
-	CustomerName string    `json:"customer_name"`
+	OrderId      uint   `json:"order_id"`
+	CustomerName string `json:"customer_name"`
+	Items        []Item
 	OrderedAt    time.Time `json:"ordered_at"`
 }
