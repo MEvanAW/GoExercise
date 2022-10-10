0bc01bee4b9eb42c74ef606e571b0ea0d1d33943
M. Evan Anindya W <m.evan.aw@gmail.com>
2022-10-10 09:54:30 +0700

    move create and get to database package.

--- OrderApi/database/database.go
+++ OrderApi/database/database.go
@@ -1,8 +1,10 @@
 package database
 
 import (
+	"errors"
 	"fmt"
 	"log"
+	"time"
 
 	"excercise.id/orderapi/models"
 	"gorm.io/driver/postgres"
@@ -32,4 +34,55 @@ func StartDB() {
 
 func GetDB() *gorm.DB {
 	return db
+}
+
+func CreateOrder(customerName string, items []models.Item) error {
+	if db == nil {
+		return errors.New("DB hasn't started yet.")
+	}
+	Order := models.Order{
+		CustomerName: customerName,
+		Items:        items,
+		OrderedAt:    time.Now(),
+	}
+	err := db.Create(&Order).Error
+	if err != nil {
+		return errors.New(fmt.Sprintf("Error creating order data: %s", err))
+	}
+	log.Println("New Order Data: ", Order)
+	return nil
+}
+
+func GetOrderById(id uint) (models.Order, error) {
+	order := models.Order{}
+	if db == nil {
+		return order, errors.New("DB hasn't started yet.")
+	}
+	err := db.Model(&models.Order{}).Preload("Items").Take(&order, id).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return order, errors.New(fmt.Sprintf("Order with ID %d not found.", id))
+		}
+		return order, err
+	}
+	return order, nil
+}
+
+func GetOrderByIds(ids ...uint) ([]models.Order, error) {
+	if len(ids) == 1 {
+		order, err := GetOrderById(ids[0])
+		return []models.Order{order}, err
+	}
+	if db == nil {
+		return nil, errors.New("DB hasn't started yet.")
+	}
+	orders := make([]models.Order, 2)
+	err := db.Model(&models.Order{}).Preload("Items").Find(&orders, ids).Error
+	if err != nil {
+		return nil, err
+	}
+	if len(orders) == 0 {
+		return nil, errors.New("No ID match.")
+	}
+	return orders, nil
 }
--- OrderApi/main.go
+++ OrderApi/main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"log"
-	"time"
 
 	"excercise.id/orderapi/database"
-	"excercise.id/orderapi/models"
-	"gorm.io/gorm"
+	_ "excercise.id/orderapi/models"
 )
 
 func main() {
@@ -16,28 +13,28 @@ func main() {
 	// CREATE
 	// items := []models.Item{
 	// 	{
-	// 		ItemCode: "100215",
-	// 		Quantity: 2,
+	// 		ItemCode: "100216",
+	// 		Quantity: 3,
 	// 	},
 	// 	{
-	// 		ItemCode:    "106311",
-	// 		Description: "on discount",
+	// 		ItemCode:    "106312",
+	// 		Description: "using promo",
 	// 		Quantity:    4,
 	// 	},
 	// }
-	// err := createOrder("Fandi", items)
+	// err := database.CreateOrder("Irfan", items)
 	// if err != nil {
 	// 	log.Println(err)
 	// }
 	// GET WHERE ID
-	// order, err := getOrderById(3)
+	// order, err := database.GetOrderById(4)
 	// if err == nil {
 	// 	log.Printf("Order data: %+v\n", order)
 	// } else {
 	// 	log.Println(err)
 	// }
 	// GET WHERE ID IN
-	orders, err := getOrderByIds([]uint{4, 5}...)
+	orders, err := database.GetOrderByIds([]uint{5, 6}...)
 	if err == nil {
 		log.Printf("Orders data:\n")
 		for _, order := range orders {
@@ -46,49 +43,4 @@ func main() {
 	} else {
 		log.Println(err)
 	}
-}
-
-func createOrder(customerName string, items []models.Item) error {
-	db := database.GetDB()
-	Order := models.Order{
-		CustomerName: customerName,
-		Items:        items,
-		OrderedAt:    time.Now(),
-	}
-	err := db.Create(&Order).Error
-	if err != nil {
-		return errors.New(fmt.Sprintf("Error creating order data: %s", err))
-	}
-	log.Println("New Order Data: ", Order)
-	return nil
-}
-
-func getOrderById(id uint) (models.Order, error) {
-	db := database.GetDB()
-	order := models.Order{}
-	err := db.Model(&models.Order{}).Preload("Items").Take(&order, id).Error
-	if err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return order, errors.New(fmt.Sprintf("Order with ID %d not found.", id))
-		}
-		return order, err
-	}
-	return order, nil
-}
-
-func getOrderByIds(ids ...uint) ([]models.Order, error) {
-	if len(ids) == 1 {
-		order, err := getOrderById(ids[0])
-		return []models.Order{order}, err
-	}
-	db := database.GetDB()
-	orders := make([]models.Order, 2)
-	err := db.Model(&models.Order{}).Preload("Items").Find(&orders, ids).Error
-	if err != nil {
-		return nil, err
-	}
-	if len(orders) == 0 {
-		return nil, errors.New("No ID match.")
-	}
-	return orders, nil
 }
