293e9d5414ea9d82f0933636a4e5ed734fa4fe85
M. Evan Anindya W <m.evan.aw@gmail.com>
2022-10-27 09:04:32 +0700

    add update photo (database) in mygram.

--- MyGram/database/photocrud.go
+++ MyGram/database/photocrud.go
@@ -37,3 +37,35 @@ func GetAllPhotos() ([]models.Photo, error) {
 	}
 	return photos, nil
 }
+
+func GetSinglePhoto(photoID uint) (models.Photo, error) {
+	photo := models.Photo{}
+	if db == nil {
+		return photo, ErrDbNotStarted
+	}
+	err := db.Model(&models.Photo{}).Take(&photo, photoID).Error
+	return photo, err
+}
+
+func UpdatePhoto(photoID uint, photoDto *dto.Photo) (UpdatedAt time.Time, err error) {
+	photo, err := GetSinglePhoto(photoID)
+	if err != nil {
+		var temp time.Time
+		return temp, err
+	}
+	if photoDto.Title != "" {
+		photo.Title = photoDto.Title
+	}
+	if photoDto.PhotoUrl != "" {
+		photo.PhotoUrl = photoDto.PhotoUrl
+	}
+	photo.Caption = photoDto.Caption
+	photo.UpdatedAt = time.Now()
+	err = db.Save(&photo).Error
+	if err == nil {
+		log.Printf("Photo Updated: %+v\n", photo)
+	} else {
+		log.Printf("Failed Update Photo %+v with %+v because of %q\n", photo, photoDto, err.Error())
+	}
+	return photo.UpdatedAt, err
+}
